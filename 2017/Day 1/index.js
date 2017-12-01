7ef378cfb1d89d180af9f039f6278466fd13b4b9
Andre Theriault <tedre191@gmail.com>
2017-12-01 16:39:40 -0500

    Better Dat 1 solution, no duplicated code

--- 2017/Day 1/index.js
+++ 2017/Day 1/index.js
@@ -6,16 +6,23 @@ fs.readFile('data.txt', 'UTF-8', (err, data) => {
         throw new Error(err);
     }
 
-    solvePart1(data);
-    solvePart2(data);
+    // Solves part one
+    // Current digit is compared to the next one (modulo to reset to 0 if you are at the end of the list)
+    var partOneAnswer = solver(data, (currentIndex, totalLenght) => (currentIndex + 1) % totalLenght);
+
+    // Solves part one
+    // Current digit is compared to the digit halfway around the circular list (modulo to reset to 0 if you are at the end of the list)
+    var partTwoAnswer = solver(data, (currentIndex, totalLenght) => (currentIndex + (totalLenght / 2)) % totalLenght);
+
+    console.log(`Part one: ${partOneAnswer}`);
+    console.log(`Part two: ${partTwoAnswer}`);
 });
 
 
-function solvePart1(data) {
+function solver(data, computeNextValue) {
     var totalSum = Array.from(data).reduce((currentSum, value, i) => {
-        // Compute the next index to check
-        // If it's the last digit, we need to go back at the beginning
-        var nextIndex = (i + 1) % (data.length);
+        // Compute the next index to check based on the challenge
+        var nextIndex = computeNextValue(i, data.length);
 
         // If the next digit is the same, we add it.
         if (Number(value) == Number(data[nextIndex])) {
@@ -25,22 +32,5 @@ function solvePart1(data) {
         return currentSum;
     }, 0);
 
-    console.log(`Part #1 answer: ${totalSum}`);
-}
-
-function solvePart2(data) {
-    var totalSum = Array.from(data).reduce((currentSum, value, i) => {
-        // Compute the next index to check
-        // If it's the last digit, we need to go back at the beginning
-        var nextIndex = (i + (data.length / 2)) % (data.length);
-
-        // If the next digit is the same, we add it.
-        if (Number(value) == Number(data[nextIndex])) {
-            currentSum += Number(value);
-        }
-
-        return currentSum;
-    }, 0);
-
-    console.log(`Part #2 answer: ${totalSum}`);
+    return totalSum;
 }
